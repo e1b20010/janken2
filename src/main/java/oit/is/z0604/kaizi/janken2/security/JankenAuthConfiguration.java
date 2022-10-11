3679a099dce8de6bdf7c90d839e222977c511212
e1b20010 <e1b20010@st.oit.ac.jp>
2022-10-11 23:57:35 +0900

    Dependency Injectionを用いたオブジェクトの共有

--- src/main/java/oit/is/z0604/kaizi/janken2/security/JankenAuthConfiguration.java
+++ src/main/java/oit/is/z0604/kaizi/janken2/security/JankenAuthConfiguration.java
@@ -13,20 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class JankenAuthConfiguration {
-  /**
-   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
-   *
-   * @return
-   */
   @Bean
   public InMemoryUserDetailsManager userDetailsService() {
-    // このクラスの下部にあるPasswordEncoderを利用して，ユーザのビルダ（ログインするユーザを作成するオブジェクト）を作成する
     UserBuilder users = User.builder();
 
-    // UserBuilder usersにユーザ名，パスワード，ロールを指定してbuildする
-    // このときパスワードはBCryptでハッシュ化されている．
-    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
-    // $ sshrun htpasswd -nbBC 10 user1 p@ss
     UserDetails user1 = users
         .username("user1")
         .password("$2y$10$Be5UlEWL/KsSo0UNO4phYuMpj/zeX7KLLhg1luEegjP6qpna9X7Ri")
@@ -37,38 +27,20 @@ public class JankenAuthConfiguration {
         .password("$2y$10$2ZUK0wrxgWoPThXxTVMEpulE.ouoT0Xd0bRS7kPV9MoAF0hl/DH7e")
         .roles("USER")
         .build();
-    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
     return new InMemoryUserDetailsManager(user1, user2);
   }
 
-  /**
-   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
-   *
-   * @param http
-   * @return
-   * @throws Exception
-   */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    // Spring Securityのフォームを利用してログインを行う（自前でログインフォームを用意することも可能）
     http.formLogin();
 
-    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
-    // mvcMatchers().authenticated()がmvcMatchersに指定されたアクセス先に認証処理が必要であることを示す
-    // authenticated()の代わりにpermitAll()と書くと認証不要となる
     http.authorizeHttpRequests()
         .mvcMatchers("/janken/**").authenticated();
 
-    http.logout().logoutSuccessUrl("/"); // ログアウト時は "http://localhost:8000/" に戻る
+    http.logout().logoutSuccessUrl("/");
     return http.build();
   }
 
-  /**
-   *
-   * UserBuilder users = User.builder();で利用するPasswordEncoderを指定する．
-   *
-   * @return BCryptPasswordEncoderを返す
-   */
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
